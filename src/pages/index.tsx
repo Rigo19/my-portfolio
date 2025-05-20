fd4ff9a3f501e2e6a633373874e969bd427c41b1
Rigo Lizaola <nwl21@txstate.edu>
2025-05-19 20:37:18 -0500

    Developed the Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,26 @@ import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
-    <div>
+
+    <div 
+      className="min-h-screen bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: "url('/homePage.jpg')" }}
+    >
+      
       <Navbar />
-      <h1>Welcome to my porfolio</h1>
+
+      <div className="flex flex-col items-start justify-center h-screen pl-8">
+        <h1 className="text-5xl font-normal text-white">
+          Rigoberto
+        </h1>
+        <h1 className="text-8xl font-bold text-white">
+          Lizaola
+        </h1>
+        <h1 className="text-3xl font-thin text-white">
+          Full Stack Developer
+        </h1>
+
+      </div>
     </div>
   )
 }
