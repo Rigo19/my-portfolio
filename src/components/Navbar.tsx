061f9ea90e8474a41facb723fa87b84224049135
Rigo Lizaola <nwl21@txstate.edu>
2025-05-20 19:55:50 -0500

    Highlights current page in Navbar using active underline

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,8 @@
 //next.js prefetches the link page in the background, so when the user clicks it
 //its already loaded
 import Link from "next/link";
-
+//importing userRouter from Next.js
+import { useRouter } from "next/router";
 
 
 
@@ -11,20 +12,23 @@ import Link from "next/link";
             will have the:
                 Home
                 About 
-                Projects
                 Skills
+                Projects
                 Contact
 the current page youre in will be highlighted in the navbar */
 
 export default function Navbar() {
+    const router = useRouter();
+    const currentPath = router.pathname;
+
     return (
         <nav className="bg-black shadow-md" role="navigation">
             <ul className="flex justify-start space-x-8 text-white font-medium list-none">
-                <li><Link href="/" className="hover:text-blue-600 cursor-pointer">Home</Link></li>
-                <li><Link href="/about" className="hover:text-blue-600 cursor-pointer">About</Link></li>
-                <li><Link href="/skills" className="hover:text-blue-600 cursor-pointer">Skills</Link></li>
-                <li><Link href="/projects" className="hover:text-blue-600 cursor-pointer">Projects</Link></li>
-                <li><Link href="/contact" className="hover:text-blue-600 cursor-pointer">Contact</Link></li>
+                <li><Link href="/" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/" ? "underline decoration-blue-600" : ""}`}>Home</Link></li>
+                <li><Link href="/about" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/about" ? "underline decoration-blue-600" : ""}`}>About</Link></li>
+                <li><Link href="/skills" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/skills" ? "underline decoration-blue-600" : ""}`}>Skills</Link></li>
+                <li><Link href="/projects" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/projects" ? "underline decoration-blue-600" : ""}`}>Projects</Link></li>
+                <li><Link href="/contact" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/contact" ? "underline decoration-blue-600" : ""}`}>Contact</Link></li>
                 
             </ul>
         </nav>
