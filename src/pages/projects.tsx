0722f158d298b4011f6900924f69eb43682e4d84
Rigo Lizaola <nwl21@txstate.edu>
2025-05-22 00:56:36 -0500

    Imported ProjectCard component into the Project page file, adding Play4Progress project to the page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,24 @@
+import Navbar from "../components/Navbar"
+import ProjectCard from "@/components/ProjectCard"
+
+export default function Projects() {
+  return (
+
+    <div className="min-h-screen bg-black text-white">
+      
+        <Navbar />
+
+
+        {/*Project Page Title*/}
+        <div className="w-full flex justify-center py-20">
+            <h1 className="text-5xl font-bold mb-4 underline decoration-blue-600 text-center">
+                Projects
+            </h1>
+        </div>
+        
+        <ProjectCard />
+        
+  
+    </div>
+  )
+}
