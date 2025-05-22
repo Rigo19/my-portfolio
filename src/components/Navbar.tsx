23f64ee5c33ad37e90d3baf5e7505aaa0ad1f338
Rigo Lizaola <nwl21@txstate.edu>
2025-05-22 02:37:42 -0500

    Made Nabar fixed and changed color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
     const currentPath = router.pathname;
 
     return (
-        <nav className="bg-black shadow-md" role="navigation">
+        <nav className="bg-stone-800 fixed top-0 left-0 w-full z-50 shadow-md" role="navigation">
             <ul className="flex justify-start space-x-8 text-white font-medium list-none">
                 <li><Link href="/" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/" ? "underline decoration-blue-600" : ""}`}>Home</Link></li>
                 <li><Link href="/about" className={`hover:text-blue-600 cursor-pointer ${currentPath === "/about" ? "underline decoration-blue-600" : ""}`}>About</Link></li>
