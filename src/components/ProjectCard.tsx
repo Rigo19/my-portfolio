c37adc041c3c06d81d5b0ec8d1e376814381d827
Rigo Lizaola <nwl21@txstate.edu>
2025-05-22 00:54:55 -0500

    Developed the Projects component that will eventaully populate the Projects page

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,26 @@
+
+  
+  export default function ProjectCard() {
+    const projects = [
+      { name: 'Play4Progress', description: "Full-stack interactive web platform for 1st through 3rd grade students to practice math drills in a gamified environment.", 
+        icon: <img src={"/play4progressPic.PNG"} alt="Play4Progress" className="shadow-lg"></img> },
+    ];
+  
+    return (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 justify-items-center py-2 px-6">
+        {projects.map((project, idx) => (
+        <button 
+            key={idx}
+            className="flex flex-col items-center w-full h-full px-6 py-4 bg-white text-black transition transform rounded hover:scale-105 shadow-md"
+        >
+            <div className="flex flex-col items-left text-left space-y-2">
+                <div className="items-center">{project.icon}</div>
+                <h1 className="text-lg font-bold">{project.name}</h1>
+                <p className="text-sm font-medium">{project.description}</p>
+            </div>
+        </button>
+        ))}
+      </div>
+    );
+  }
+  
